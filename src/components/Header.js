1710474eb8faf920f1d368c89466ccfe0f177fbf
Ana Andrades <162385977+anaandrades08@users.noreply.github.com>
2025-01-13 20:40:09 -0400

    Initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1 className="logo">Meu Portfólio</h1>
+      <nav>
+        <ul className="nav-links">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">Sobre Mim</Link>
+          </li>
+          <li>
+            <Link to="/portfolio">Portfólio</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contato</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
